58ad44087c95367e6f3f41c274c97486156c4314
Saveed shah <63816462+saveedshah@users.noreply.github.com>
2022-01-13 21:04:36 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function toggelPopup(){
+    document.querySelector("#popup-1").classList.toggle("active");
+ 
+}
+
+
+
+function onClickClose(){
+    
+    document.querySelector(".popup").style.display="none";
+    
+}
